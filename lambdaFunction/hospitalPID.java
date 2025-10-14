af50dd7eff42dbb347ebc6055e19fa2329e35555
Sandesh <guptask0722@gmail.com>
2025-10-15 01:53:58 +0530

    deleted lambda folder

--- lambdaFunction/hospitalPID.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class hospitalPID {
-    public static void main(String[] args) {
-        List<String> patientIDs = Arrays.asList("P101", "P102", "P103", "P104");
-        patientIDs.forEach(System.out::println);
-    }
-}
